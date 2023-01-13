a85ba2e42cdaf4c0339834fd2e66509e75a28e39
Abdul Moiz Anwer <89318559+AbdulMoizAnwer@users.noreply.github.com>
2023-01-13 21:38:25 +0500

    Create index4.js

---
+++ index4.js
@@ -0,0 +1,12 @@
+// Write a program to swap two numbers
+var a = 5;
+var b = 10;
+
+//output b=5; a=10;
+
+var c = b; // c = 10
+b = a; // b = 5
+a = c // a = 10
+
+console.log("the value of a is " + a);
+console.log("the value of b is " + b);
